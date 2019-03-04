f7063aed32c19ae244b997b62b71f089ec10ef99
Patryk Ostasz <patryk.ostasz@gmail.com>
2019-03-04 23:14:19 +0100

    SDL2: Initial Commit
    What's new:
     - Game Loop
     - Loading sprites and textures
     - Limiting FrameRate

---
+++ sources/Game.cpp
@@ -0,0 +1,74 @@
+#include "Game.h"
+#include "SDL_image.h"
+
+#include <iostream>
+
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+Game::Game() {
+
+}
+
+Game::~Game() {
+
+}
+
+void Game::init(const char* title, int x, int y, int width, int height, bool fullscreen) {
+	int flags = 0;
+	if (fullscreen) {
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		std::cout << "Subsystems Initialized!..." << std::endl;
+
+		window = SDL_CreateWindow(title, x, y, width, height, flags);
+		if (window) {
+			std::cout << "Window created!" << std::endl;
+		}
+
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 1);
+			std::cout << "Renderer created!" << std::endl;
+		}
+
+		isRunning = true;
+
+	} else {
+		isRunning = false;
+	}
+    SDL_Surface* tmpSurface = IMG_Load("./assets/mario_one.jpg");
+	playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
+}
+void Game::handleEvents() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+void Game::update() {
+    destR.h = 64;
+    destR.w = 64;
+    destR.x++;
+}
+void Game::render() {
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
+	SDL_RenderPresent(renderer);
+}
+void Game::clean() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::cout << "Game cleaned!" << std::endl;
+}
+bool Game::running() {
+	return isRunning;
+}
---
+++ sources/main.cpp
@@ -0,0 +1,29 @@
+#include "Game.h"
+
+int main(int argc, char** argv) {
+
+    const int FPS = 60;
+    const int frameDelay = 1000 / FPS;
+
+    uint32_t frameStart;
+    int frameTime;
+
+
+    Game* game = new Game();
+	game->init("Hej", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, false);
+	while (game->running()) {
+
+        frameStart = SDL_GetTicks();
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+		if (frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+		}
+	}
+	game->clean();
+	return 0;
+}
